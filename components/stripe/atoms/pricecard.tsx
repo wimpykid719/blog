57339c868f08ba16b6c2bbea3aa99ea6f366a255
“wataru” <ltomu.web@gmail.com>
2022-03-03 22:51:19 +0900

    feat: グラデーションを追加
    https://www.joshwcomeau.com/gradient-generator/
    上記のサイトで作成したグラデーションを追加する

--- components/stripe/atoms/pricecard.tsx
+++ components/stripe/atoms/pricecard.tsx
@@ -6,16 +6,18 @@ export default function PriceCard({ id, amount, donateId, onChange }: PriceCardP
       <input
         type="radio"
         id={`price${id}`}
-        className="hidden"
+        className="hidden hidden-after"
         value={id} onChange={onChange}
         checked={id === donateId}
       />
       <label
         htmlFor={`price${id}`}
-        className={`block w-4/5 mx-auto h-48 rounded-3xl  shadow-sm ${id === donateId ? "custom-gradient" : "bg-white"} transition ease-in-out delay-300`}
+        className={`block w-4/5 mx-auto h-48 rounded-3xl shadow-sm relative z-50`}
       >
-        <span className={`block w-3 h-3 ${id === donateId ? "bg-white" : "custom-gradient"} rounded-full transition ease-in-out delay-300`}></span>
+        <span className={`block absolute z-10 w-3 h-3 ${id === donateId ? "bg-white" : "bg-blue"} rounded-full transition-all ease-in-out delay-300`}></span>
         {`${amount}円`}
+        <span className={`block rounded-3xl custom-white ${id === donateId && "opacity-0"}`} />
+        <span className={`block rounded-3xl custom-gradient${id}`} />
       </label>
     </>
   )
