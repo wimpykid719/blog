6e1ebdfefaffd5621b3e8e180e3186449b6686aa
“wataru” <ltomu.web@gmail.com>
2022-03-27 11:42:22 +0900

    chore: クラス名が全て繋がっていないとコンパイル時に出力されないか確認

--- components/stripe/atoms/pricecard.tsx
+++ components/stripe/atoms/pricecard.tsx
@@ -5,11 +5,11 @@ export default function PriceCard({ id, amount, donateId, message, onChange }: P
   const gradientColor = ((id) => {
     switch(id) {
       case "1":
-        return "orange"
+        return "custom-orange"
       case "2":
-        return "blue"
+        return "custom-blue"
       case "3":
-        return "yellow"
+        return "custom-yellow"
     }
   })(id)
   return(
@@ -26,14 +26,14 @@ export default function PriceCard({ id, amount, donateId, message, onChange }: P
         className={`block w-3/5 max-w-xs mb-7 h-44 rounded-3xl shadow-sm relative z-50`}
       >
         <span className="block absolute z-1 w-full p-3">
-          <span className={`block mb-2 w-5 h-5 ${id === donateId ? "bg-white" :`custom-${gradientColor}`} rounded-full inner-white`}></span>
+          <span className={`block mb-2 w-5 h-5 ${id === donateId ? "bg-white" : gradientColor} rounded-full inner-white`}></span>
           <span className="block bg-white bg-opacity-80 rounded-3xl p-3">
             <span className="block text-5xl">{`${amount}`}<small className="text-2xl">円</small></span>
             <span className="block">{message}</span>
           </span>
         </span>
         <span className={`block rounded-3xl custom-white ${id === donateId ? "opacity-0" : "opacity-1"}`} />
-        <span className={`block rounded-3xl position-gradient custom-${gradientColor} ${id === donateId ? "opacity-1" : "opacity-0"}`} />
+        <span className={`block rounded-3xl position-gradient ${gradientColor} ${id === donateId ? "opacity-1" : "opacity-0"}`} />
       </label>
     </>
   )
