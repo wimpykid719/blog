6f8540db1e8b3fba01b8c116278d7e973e1bf0f9
“wataru” <ltomu.web@gmail.com>
2022-04-04 20:12:37 +0900

    refactor: gradientColorをフック化しました

---
+++ lib/utility/gradientColor.ts
@@ -0,0 +1,10 @@
+export const gradientColor = (id:string) => {
+  switch(id) {
+    case "1":
+      return "custom-orange"
+    case "2":
+      return "custom-blue"
+    case "3":
+      return "custom-yellow"
+  }
+};
