cc753bf2ad25f75a434630c458ae6333f3b83959
“wataru” <ltomu.web@gmail.com>
2022-04-04 20:19:51 +0900

    refactor: gradientColorのフックを削除
    tailwindcssのコンパイルでファイル内にクラス名が存在しないのでcssが出力されなくなる。

--- components/stripe/atoms/pricecard.tsx
+++ components/stripe/atoms/pricecard.tsx
@@ -1,9 +1,17 @@
 import { PriceCardProps } from '../../../types/stripe/PriceCard'
-import { gradientColor } from '../../../lib/utility/gradientColor'
 
 export default function PriceCard({ id, amount, donateId, message, onChange }: PriceCardProps) {
   // 数値がtailwindwind cssがコンパイルする際に使用出来ないため
-  const gradientColorClassName = gradientColor(id)
+  const gradientColor = ((id) => {
+    switch(id) {
+      case "1":
+        return "custom-orange"
+      case "2":
+        return "custom-blue"
+      case "3":
+        return "custom-yellow"
+    }
+  })(id)
   return(
     <>
       <input
@@ -18,14 +26,14 @@ export default function PriceCard({ id, amount, donateId, message, onChange }: P
         className={`block w-3/5 max-w-xs mb-7 h-44 rounded-3xl shadow-sm relative z-50`}
       >
         <span className="block absolute z-1 w-full p-3">
-          <span className={`block mb-2 w-5 h-5 ${id === donateId ? "bg-white" : gradientColorClassName} rounded-full inner-white`}></span>
+          <span className={`block mb-2 w-5 h-5 ${id === donateId ? "bg-white" : gradientColor} rounded-full inner-white`}></span>
           <span className="block bg-white bg-opacity-80 rounded-3xl p-3">
             <span className="block text-5xl">{`${amount}`}<small className="text-2xl">円</small></span>
             <span className="block">{message}</span>
           </span>
         </span>
         <span className={`block rounded-3xl custom-white ${id === donateId ? "opacity-0" : "opacity-1"}`} />
-        <span className={`block rounded-3xl position-gradient ${gradientColorClassName} ${id === donateId ? "opacity-1" : "opacity-0"}`} />
+        <span className={`block rounded-3xl position-gradient ${gradientColor} ${id === donateId ? "opacity-1" : "opacity-0"}`} />
       </label>
     </>
   )
