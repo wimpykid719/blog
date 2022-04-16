a9eb35f49324d4a0cc18b0fe56a7121e5e80af65
“wataru” <ltomu.web@gmail.com>
2022-04-16 15:19:37 +0900

    fix: Stripe決済完了後のリダイレクトURLの切り替え処理
    切り替え処理の判定に使用していた値がビルド時にしか有効にならないため
    うまく判定ができないのでとりあえずは本番のURLを当てる事で対応する

--- techBlogSettings/aboutblog.ts
+++ techBlogSettings/aboutblog.ts
@@ -1,10 +1,5 @@
 export const aboutblog = {
-  url: (() => {
-    if (process.env.PRODUCTION) {
-      return 'https://techblog-pink.vercel.app/'
-    }
-    return 'http://localhost:3000/'
-  })(),
+  url: 'https://techblog-pink.vercel.app/',
   title: "大学生だった",
   description: "Zennに投稿した記事を使用して作成したオリジナルブログ、プログラミング技術に関する内容を投稿します。",
 }
