4afdbd5319975b9164be0deea309f40e3a38a859
wataru <ltomu.web@gmail.com>
2024-08-26 11:17:20 +0900

    fix: usePageView廃止
    ga4になり以前の計測方法が廃止となったため

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.css'
 import '../styles/prism-daigakusei.css'
 import 'katex/dist/katex.min.css'
-import usePageView from '../src/hooks/usePageView'
 import { AppProps } from 'next/app'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  usePageView()
   return <Component {...pageProps} />
 }
 
